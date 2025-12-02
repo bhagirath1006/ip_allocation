521411ab55007805324f7948946584a604dc7f3a
DevOps Team <devops@example.com>
2025-12-02 18:53:24 +0530

    Simplify EC2 launch

--- terraform/modules/ec2/main.tf
+++ terraform/modules/ec2/main.tf
@@ -21,27 +21,39 @@ resource "aws_network_interface" "secondary" {
   }
 }
 
-# EC2 Instances - Use pre-created network interfaces
+# EC2 Instances - Simplified: Primary ENI only, secondary attached after
 resource "aws_instance" "main" {
   count         = var.instance_count
   ami           = var.ami
   instance_type = var.instance_type
-  monitoring    = true
-
-  # Primary network interface (device index 0)
-  network_interface {
-    network_interface_id = aws_network_interface.primary[count.index].id
-    device_index         = 0
-  }
+  monitoring    = false
+  
+  # Use primary network interface only for launch
+  subnet_id              = var.subnet_id
+  vpc_security_group_ids = [var.security_group_id]
+  
+  # Disable source/dest check to allow multi-IP routing
+  source_dest_check = false
 
   tags = {
     Name = "${var.project_name}-instance-${count.index + 1}"
   }
 
+  # Ensure ENIs are created first
   depends_on = [
     aws_network_interface.primary,
     aws_network_interface.secondary
   ]
+}
+
+# Attach Primary Network Interfaces to instances (as additional interfaces, not primary)
+resource "aws_network_interface_attachment" "primary" {
+  count                = var.instance_count
+  instance_id          = aws_instance.main[count.index].id
+  network_interface_id = aws_network_interface.primary[count.index].id
+  device_index         = 1
+
+  depends_on = [aws_instance.main]
 }
 
 # Attach Secondary Network Interfaces to instances
@@ -49,9 +61,9 @@ resource "aws_network_interface_attachment" "secondary" {
   count                = var.instance_count
   instance_id          = aws_instance.main[count.index].id
   network_interface_id = aws_network_interface.secondary[count.index].id
-  device_index         = 1
+  device_index         = 2
 
-  depends_on = [aws_instance.main]
+  depends_on = [aws_network_interface_attachment.primary]
 }
 
 # Elastic IP for Primary Network Interface
