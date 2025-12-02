a4abda93d8da9eac26929b41017b3d653fa1107d
DevOps Team <devops@example.com>
2025-12-02 18:57:39 +0530

    Remove extra brace

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -47,7 +47,7 @@ variable "instance_type" {
   description = "EC2 instance type"
   type        = string
   default     = "t3.small"
-}}
+}
 
 variable "ami" {
   description = "AMI ID for EC2 instances (Amazon Linux 2)"
